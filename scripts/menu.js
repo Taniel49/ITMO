7e7ed84eac613809ce17fe6d15c6713f23be9cb2
Balai Mikheeva <balaishka@yandex.ru>
2022-04-01 16:02:42 +0300

    feat: Добавить шапку, написать скрипт для меню, адаптивная верстка

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,46 +1,107 @@
-const page = document.querySelector('.page');
+const page = document.querySelector(".page");
+const content = page.querySelector(".content");
+const footer = page.querySelector(".footer");
 
-const burger = page.querySelector('.burger');
-const burgerLinesList = Array.from(burger.querySelectorAll('.burger__line'));
+const burger = page.querySelector(".burger");
+const burgerLinesList = Array.from(burger.querySelectorAll(".burger__line"));
 
-const menu = page.querySelector('.menu');
-const linksList = Array.from(menu.querySelectorAll('.menu__link'));
-const linkOpener = menu.querySelector('.menu__link_type_opener');
-const submenu = menu.querySelector('.submenu');
+const menu = page.querySelector(".menu");
+const linksList = Array.from(menu.querySelectorAll(".menu__link"));
+const linkOpener = menu.querySelector(".menu__link_type_opener");
+const submenu = menu.querySelector(".submenu");
 
-// Функция плавного выпадения/скрытия меню
+const heightSubmenu = submenu.offsetHeight; // Высота подменю
+const heightMenu = menu.offsetHeight; // Высота меню
+const heightMenuWithoutSubmenu = menu.offsetHeight - heightSubmenu; // Высота меню с закрытым подменю
+
+// Обнуление высоты меню
+function resetHeightMenu(menu) {
+  menu.style.height = "0px";
+}
+
+// Фукнкция открытия меню
+function openMenu(menu) {
+  menu.classList.add(`${menu.id}_open`);
+  menu.style.height = `${menu.scrollHeight}px`;
+}
+
+// Фукнкция закрытия меню
+function closeMenu(menu) {
+  menu.classList.remove(`${menu.id}_open`);
+  menu.style.height = "0px";
+}
+
+// Функция открытия/закрытия меню
 function toggleMenu(menu) {
-  const classOpenName = menu.id + '_open';
-
-  if (!menu.classList.contains(classOpenName)) {
-    menu.classList.add(classOpenName);
+  if (!menu.classList.contains(`${menu.id}_open`)) {
+    openMenu(menu);
   } else {
-    menu.classList.remove(classOpenName);
+    closeMenu(menu);
   }
 }
 
-// Функция выпадения/скрытия меню навигации
+// Функция изменения высоты меню
+function changeHeightMenu() {
+  if (submenu.classList.contains("submenu_open")) {
+    menu.style.height = heightMenu + "px";
+  } else {
+    menu.style.height = heightMenuWithoutSubmenu + "px";
+  }
+}
+
+// Функция открытия/закрытия меню навигации
 function toggleNavMenu() {
-  burger.classList.toggle('burger_open');
+  burger.classList.toggle("burger_open");
 
   burgerLinesList.forEach((line) => {
-    line.classList.toggle('burger__line_open');
+    line.classList.toggle("burger__line_open");
   });
 
   toggleMenu(menu);
 }
 
-// Функция выпадения/скрытия подменю навигации
+// Функция открытия/закрытия подменю навигации
 function toggleSubmenu() {
-  linkOpener.classList.toggle('menu__link_open');
+  linkOpener.classList.toggle("menu__link_open");
 
   linksList.forEach((link) => {
-    link.classList.toggle('menu__link_inactive');
+    link.classList.toggle("menu__link_inactive");
+    link.toggleAttribute("disabled");
   });
 
   toggleMenu(submenu);
+
+  changeHeightMenu();
 }
 
-burger.addEventListener('click', toggleNavMenu);
+// // Функция открытия/закрытия меню по клику вне элемента
+function closeMenus() {
+  burger.classList.remove("burger_open");
+
+  burgerLinesList.forEach((line) => {
+    line.classList.remove("burger__line_open");
+  });
+
+  closeMenu(submenu);
+
+  linkOpener.classList.remove("menu__link_open");
+
+  linksList.forEach((link) => {
+    link.classList.remove("menu__link_inactive");
+    link.removeAttribute("disabled");
+  });
+
+  closeMenu(menu);
+}
+
+burger.addEventListener("click", toggleNavMenu);
 
 linkOpener.addEventListener("click", toggleSubmenu);
+
+content.addEventListener("click", closeMenus);
+
+footer.addEventListener("click", closeMenus);
+
+resetHeightMenu(menu);
+
+resetHeightMenu(submenu);
